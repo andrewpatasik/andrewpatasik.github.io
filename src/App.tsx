01c782a9aba66673402140efa2281f548d0a56d8
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2023-05-24 12:26:52 +0800

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer"
 import { Navbar } from "./components/Navbar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <div style={{fontFamily: 'inter, sans-serif'}} className="flex flex-col w-3/5 mx-auto">
       <Navbar />
+      <Footer />
     </div>
   )
 }
