29c8bae5b3cf83984096a08f9ce03f42f1827846
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2023-05-24 12:22:44 +0800

    project setup and navbar

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./components/Navbar"
+
+function App() {
+
+  return (
+    <div style={{fontFamily: 'inter, sans-serif'}} className="flex flex-col w-3/5 mx-auto">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
